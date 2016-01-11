9112f9f0e01e1732d84a25b66b51d238d88a28c3
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-11 13:53:07 +0900

    init

---
+++ Assets/Scripts/GameContrller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameContrller : MonoBehaviour {
+	public GameObject charactorObject;
+
+	AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+				if (hit.collider.gameObject.tag == "Char01") {
+					sound.Play();
+				}
+			}
+		}
+	}
+}
